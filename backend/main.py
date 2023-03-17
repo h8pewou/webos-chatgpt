04d0acc7b5102d31cdb178d8d4a1fd5f3fa96134
Adam Maracz <adam.maracz@me.com>
2023-03-17 20:39:10 +0100

    Fixed chat log

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import json
 import array
 
 openai.api_key = "YOUR_API_KEY"
+chat_log = []
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -12,37 +13,41 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def chat():
     message = request.form["message"]
-    chat_log = request.form.get("chat_log")
+    global chat_log
 
     # Parse chat_log as a list of messages if it exists, else start with an empty list
-    messages = json.loads(chat_log) if chat_log else []
+    print("### Chat log start")
+    print(chat_log)
+    messages = []
+    messages = chat_log
 
     # Add the user's message to the messages array
-    messages.append({"role": "user", "content": message})
+    new_message = {"role": "user", "content": message}
+    messages.append(new_message)
+
     # Build the input to the OpenAI API
-    input_text = json.dumps([{"role": "user", "content": message}])
-    print(input_text)
+    input_text = json.dumps(messages)
 
     # Get the response from the OpenAI API
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=input_text,
+        messages=json.loads(input_text),
         temperature=0.7,
         max_tokens=1024,
         stop=None,
     )
-    print(response)
 
     # Extract the response message and add it to the messages array
     response_message = response.choices[0].message['content'].strip()
-    messages.append({"role": "assistant", "content": response_message})
+    new_response = {"role": "assistant", "content": response_message}
+    messages.append(new_response)
 
     # Build the response to be returned to the client
     response_data = {
         "messages": messages
     }
 
-    resp = make_response(jsonify(response_data))
+    resp = make_response(jsonify({'message': response_message}))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
